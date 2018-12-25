4fe4f1e2d96f2c4374fa7ecc8b129fbfc9326a20
ryan <ryant18@vt.edu>
2018-12-24 22:19:50 -0500

    Stop login webpage from timing out

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ class App:
                 if cc.isclassopen(crn, self.termyearid):
                     print('Crn is open ' + str(crn))
                     browser.addcrn(crn)
+            browser.clickbutton(self.find_element_by_id, 'saveButton')
             self.continuecheck.wait(self.delay)
         browser.close()
 
